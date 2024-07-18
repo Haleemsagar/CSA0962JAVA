95226ee69fa6db2c6769c2b1f059071bbcc426b5
Haleemsagar <162274710+Haleemsagar@users.noreply.github.com>
2024-07-18 13:52:56 +0530

    Add files via upload

---
+++ String to Integer.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class String to Integer 
+{
+public static void main(Strin args[])
+ {
+Scanner scanner = new Scanner(System.in);
+int number = scanner.nextInt();
+System.out.println("Integer: " + number);
+}
+}
